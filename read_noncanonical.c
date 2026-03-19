a59b69de36a6c51716db4a9acf0b80aa7bccda82
berLopes <bernardomagalhaeslopes@gmail.com>
2026-03-19 16:23:41 +0000

    Add files via upload

---
+++ read_noncanonical.c
@@ -0,0 +1,186 @@
+// Read from serial port in non-canonical mode
+//
+// Modified by: Eduardo Nuno Almeida [enalmeida@fe.up.pt]
+
+#include <fcntl.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <termios.h>
+#include <unistd.h>
+
+// Baudrate settings are defined in <asm/termbits.h>, which is
+// included by <termios.h>
+#define BAUDRATE B38400
+#define _POSIX_SOURCE 1 // POSIX compliant source
+
+#define FALSE 0
+#define TRUE 1
+
+#define BUF_SIZE2 1000
+#define BUF_SIZE 5
+
+struct termios oldtio;
+struct termios newtio;
+
+int fd;
+
+volatile int STOP = FALSE;
+
+typedef enum {
+    START,
+    FLAG_RCV,
+    A_RCV,
+    C_RCV,
+    BCC_OK,
+    END
+} state;
+
+int llopen(const char *serialPortName){
+
+    int fd = open(serialPortName, O_RDWR | O_NOCTTY);
+    if (fd < 0)
+    {
+        perror(serialPortName);
+        exit(-1);
+    }
+
+    // Save current port settings
+    if (tcgetattr(fd, &oldtio) == -1)
+    {
+        perror("tcgetattr");
+        exit(-1);
+    }
+
+    // Clear struct for new port settings
+    memset(&newtio, 0, sizeof(newtio));
+
+    newtio.c_cflag = BAUDRATE | CS8 | CLOCAL | CREAD;
+    newtio.c_iflag = IGNPAR;
+    newtio.c_oflag = 0;
+
+    // Set input mode (non-canonical, no echo,...)
+    newtio.c_lflag = 0;
+    newtio.c_cc[VTIME] = 0; // Inter-character timer unused
+    newtio.c_cc[VMIN] = 5;  // Blocking read until 5 chars received
+
+    // VTIME e VMIN should be changed in order to protect with a
+    // timeout the reception of the following character(s)
+
+    // Now clean the line and activate the settings for the port
+    // tcflush() discards data written to the object referred to
+    // by fd but not transmitted, or data received but not read,
+    // depending on the value of queue_selector:
+    //   TCIFLUSH - flushes data received but not read.
+    tcflush(fd, TCIOFLUSH);
+
+    // Set new port settings
+    if (tcsetattr(fd, TCSANOW, &newtio) == -1)
+    {
+        perror("tcsetattr");
+        exit(-1);
+    }
+
+    printf("New termios structure set\n");
+
+    return 0;
+}
+
+
+
+int main(int argc, char *argv[])
+{
+    // Program usage: Uses either COM1 or COM2
+    const char *serialPortName = argv[1];
+
+    if (argc < 2)
+    {
+        printf("Incorrect program usage\n"
+               "Usage: %s <SerialPort>\n"
+               "Example: %s /dev/ttyS1\n",
+               argv[0],
+               argv[0]);
+        exit(1);
+    }
+
+    llopen(serialPortName);
+
+    // Open serial port device for reading and writing and not as controlling tty
+    // because we don't want to get killed if linenoise sends CTRL-C.
+    
+    // Loop for input
+    unsigned char buf2[BUF_SIZE2 + 1] = {0};
+    unsigned char buf[BUF_SIZE + 1] = {0}; // +1: Save space for the final '\0' char
+
+    state currentstate = START;
+        while (STOP==FALSE)
+        {
+            read(fd, buf, 1);
+            printf("var = 0x%02X\n", (unsigned int)(buf[0] & 0xFF));
+
+            switch(currentstate){
+                case START:
+                 if(buf[0]==0x7E){
+                    currentstate = FLAG_RCV;
+                    }
+                    break;
+                case FLAG_RCV:
+                 if(buf[0]==0x03){
+                    currentstate = A_RCV;
+                }
+                    break;
+                case A_RCV:
+                 if(buf[0]==0x03){
+                    currentstate = C_RCV;
+                }
+                    break;
+                case C_RCV:
+                 if(buf[0]==(0x03 ^ 0x03)){
+                    currentstate = BCC_OK;
+                    printf("to bcc ok\n");
+                }
+                    break;
+                case BCC_OK:
+                 if(buf[0]==0x7E){
+                    currentstate = END;
+                    STOP=TRUE;
+                    printf("end\n");
+                }
+                    break;
+                case END:
+                    break;
+            }
+        }
+        
+      //  buf[bytes] = '\0'; // Set end of string to '\0', so we can printf
+
+  /*      for (int i = 0; i < 5; i++)
+        {
+            printf("var = 0x%02X\n", (unsigned int)(buf[i] & 0xFF));
+
+        }*/
+
+    buf[0] = 0x7E;
+    buf[1] = 0x03;
+    buf[2] = 0x03;
+    buf[3] = 0x03 ^ 0x03;
+    buf[4] = 0x7E;
+
+    write(fd, buf, BUF_SIZE);
+    printf("send\n");
+    // The while() cycle should be changed in order to respect the specifications
+    // of the protocol indicated in the Lab guide
+    sleep(1);
+    // Restore the old port settings
+    if (tcsetattr(fd, TCSANOW, &oldtio) == -1)
+    {
+        perror("tcsetattr");
+        exit(-1);
+    }
+
+    close(fd);
+
+    return 0;
+}
